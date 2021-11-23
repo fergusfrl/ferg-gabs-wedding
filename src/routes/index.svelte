2fc0c87dfd49f3a1a905ae6a9fd131df29978c23
Fergus Farrell <fergus@batch.nz>
2021-11-23 19:31:45 +1300

    added save the date

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,7 +11,7 @@
 </script>
 
 <svelte:head>
-	<title>Celebrate With Us</title>
+	<title>Save the Date | Gabs and Ferg</title>
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
 	<link
@@ -20,19 +20,22 @@
 	/>
 </svelte:head>
 
-<div class="flex flex-col pt-14 pb-24 space-y-5 items-center border-8 border-autumn-100">
-	<div class="flex flex-col items-center space-x-4 transform -rotate-6 text-autumn-500">
+<div class="flex flex-col pt-14 space-y-5 items-center border-8 border-autumn-100 min-h-screen">
+	<!-- <div class="flex flex-col items-center space-x-4 transform -rotate-6 text-autumn-500">
 		<h1 class="font-cursive text-5xl">Gabrielle</h1>
 		<h1 class="font-cursive text-3xl">&</h1>
 		<h1 class="font-cursive text-5xl">Fergus</h1>
-	</div>
-	<img src={image} alt="Lake Ohau with Wedding Arch" class="pt-10 py-5" />
-	<h4 class="font-serif pt-6 uppercase">are delighted to invite you to their wedding on</h4>
-	<h3 class="font-cursive text-5xl text-autumn-500">06 . 05 . 2023</h3>
-	<div class="flex flex-col items-center pt-4 pb-20 space-y-4">
+	</div> -->
+	<h4 class="font-serif pt-6 uppercase">Save the date</h4>
+	<h3 class="font-cursive text-5xl text-autumn-500">06 May, 2023</h3>
+	<img src={image} alt="Lake Ohau with Wedding Arch" class="p-5 md:p-10" />
+	<h4 class="font-serif uppercase pb-14 mx-16 text-center">We can't wait to celebrate with you</h4>
+	<!-- <h4 class="font-serif pt-6 uppercase">Save the date</h4>
+	<h3 class="font-cursive text-5xl text-autumn-500">06 May, 2023</h3> -->
+	<!-- <div class="flex flex-col items-center pt-4 pb-20 space-y-4">
 		<h4 class="font-serif uppercase">Lake Ōhau Lodge</h4>
 		<h4 class="font-serif uppercase pb-4">2295 Lake Ōhau Road, Canterbury</h4>
 		<button class="button" on:click={handleMapClick}>View on map</button>
-	</div>
-	<RsvpForm />
+	</div> -->
+	<!-- <RsvpForm /> -->
 </div>
