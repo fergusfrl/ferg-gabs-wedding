a4e91c7890cbda9a603e33ed8d1e4956b3cfd7f1
Fergus Farrell <fergus@batch.nz>
2021-11-28 15:04:12 +1300

    Added spotify function

---
+++ functions/spotify-connector.ts
@@ -0,0 +1,46 @@
+import { Handler, getSecrets, NetlifySecrets } from '@netlify/functions';
+
+const searchSpotify = async (bearerToken, searchString) => {
+	const searchStringQuery = searchString;
+	const response = await fetch(
+		`https://api.spotify.com/v1/search?limit=20&q=${searchStringQuery}`,
+		{
+			headers: {
+				Authorization: bearerToken,
+				'Content-Type': 'application/json'
+			}
+		}
+	);
+	const data = await response.json();
+
+	console.log('DATA:', data);
+
+	return data;
+};
+
+const handler: Handler = async (event) => {
+	const secrets: NetlifySecrets = await getSecrets(event);
+
+	console.log('CONTEXT:', event);
+	console.log('SECRETS:', secrets);
+
+	// Not Authenticated.
+	if (!secrets?.spotify?.isLoggedIn) {
+		return { statusCode: 401 };
+	}
+
+	// No search string provided.
+	if (!event.queryStringParameters.searchString) {
+		return { statusCode: 404 };
+	}
+
+	const bearerToken = secrets.spotify.bearerToken;
+	const songs = await searchSpotify(bearerToken, event.queryStringParameters.searchString);
+
+	return {
+		statusCode: 200,
+		body: JSON.stringify({ songs })
+	};
+};
+
+export { handler };
