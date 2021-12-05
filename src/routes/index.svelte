1a95b22fe85eda7a1da93c077a7dac2a4bbfc839
Fergus Farrell <fergus@batch.nz>
2021-12-05 16:55:46 +1300

    Added RSVP route

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,6 @@
 <script lang="ts">
-	import '../styles/tailwind.css';
 	import { GOOGLE_MAPS_LINK } from '../constants';
 	import image from '../assets/lake-ohau-lodge-wedding.jpeg';
-
-	import RsvpForm from '../components/rsvp-form.svelte';
-
-	const handleMapClick = (): void => {
-		window.open(GOOGLE_MAPS_LINK, '_blank');
-	};
 </script>
 
 <svelte:head>
@@ -20,7 +13,7 @@
 	/>
 </svelte:head>
 
-<div class="flex flex-col pt-14 space-y-5 items-center border-8 border-autumn-100 min-h-screen">
+<div class="flex flex-col py-14 space-y-5 items-center border-8 border-autumn-100 min-h-screen">
 	<div class="flex flex-col items-center space-x-4 transform -rotate-6 text-autumn-500">
 		<h1 class="font-cursive text-5xl">Gabrielle</h1>
 		<h1 class="font-cursive text-3xl">&</h1>
@@ -29,10 +22,13 @@
 	<img src={image} alt="Lake Ohau with Wedding Arch" class="p-5 md:p-10" />
 	<h4 class="font-serif uppercase">Are delighted to invite you to their wedding on</h4>
 	<h3 class="font-cursive text-3xl md:text-5xl text-autumn-500">06 May, 2023</h3>
-	<div class="flex flex-col items-center pt-2 md:pt-4 pb-20 space-y-4">
+	<a
+		href={GOOGLE_MAPS_LINK}
+		target="_blank"
+		class="flex flex-col items-center pt-2 md:pt-4 pb-1- space-y-4 underline hover:no-underline"
+	>
 		<h4 class="font-serif uppercase">Lake Ōhau Lodge</h4>
 		<h4 class="font-serif uppercase pb-4">2295 Lake Ōhau Road, Canterbury</h4>
-		<button class="button" on:click={handleMapClick}>View on map</button>
-	</div>
-	<RsvpForm />
+	</a>
+	<a href="/rsvp" class="button flex flex-row justify-between group">RSVP Here</a>
 </div>
