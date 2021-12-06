e5ae10af99ecd83d99cc665c2ec871a9942b8d1e
Fergus Farrell <fergus@batch.nz>
2021-12-07 10:09:51 +1300

    Added main prefix

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const GOOGLE_MAPS_LINK =
 	'https://www.google.com/maps/dir/-43.5531027,172.6201228/Lake+Ohau+Lodge+2295+Lake+Ohau+Road,+Lake,+Lake+%C5%8Chau+9412/@-43.9476975,170.6565388,9z/data=!3m1!4b1!4m10!4m9!1m1!4e1!1m5!1m1!1s0x6d2adee7f5389077:0xd851dd6cc6b7138e!2m2!1d169.8144528!2d-44.2286245!3e0';
-export const FUNCTIONS_BASE_URL = '.netlify/functions';
+export const FUNCTIONS_BASE_URL = 'https://main--gabsandferg.netlify.app/.netlify/functions';
