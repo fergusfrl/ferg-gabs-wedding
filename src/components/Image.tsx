dc05ba748e1ce62d39a52a5aed77b0ff94a84971
Fergus Farrell <fergusfrl@gmail.com>
2023-07-21 10:51:53 +1200

    feat: reduced landscape image width request

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -10,13 +10,14 @@ type Props = {
 const Image = ({ cloudinaryFileName, blurPlaceholder }: Props) => {
   return (
     <NextImage
-      src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_6000/${cloudinaryFileName}`}
-      width={1000}
-      height={1000}
+      src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_4000/${cloudinaryFileName}`}
+      width={0}
+      height={0}
       alt={cloudinaryFileName}
       decoding="async"
       loading="eager"
-      className="w-screen h-screen object-contain"
+      sizes="100vh"
+      className="w-full h-full object-contain"
       placeholder="blur"
       blurDataURL={blurPlaceholder}
     />
